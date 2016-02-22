0e4212cd90b9baf8f44d63f62583f5daa55e4879
Sean Connor <connorseanp@gmail.com>
2016-02-21 16:32:42 -0800

    cleaned file structure

--- config/application.rb
+++ config/application.rb
@@ -1,9 +1,7 @@
 require File.expand_path('../boot', __FILE__)
 
 require 'rails/all'
-config.assets.precompile += [
-    'welcome.scss'
-  ]
+
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
